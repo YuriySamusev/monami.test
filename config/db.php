68f8062dd8b405fdec438ea6b7d4e83b4e50147e
Yuriy <samusev.ua@gmail.com>
2017-06-23 00:35:32 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=monami_test',
     'username' => 'root',
-    'password' => '#7Y\Ww_Jdf2W8L#8D5v}z',
+    'password' => '#7Y\Ww_Jdf2W8342L#8D5v}789z',
     'charset' => 'utf8',
 ];
